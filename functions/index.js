4e3f40aa5a0a337d12ecb9891f773bdcd1af71d9
Siddhant <siddhantvinchurkar@gmail.com>
2019-10-07 01:31:14 +0530

    Begin Code

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,38 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//exports.helloWorld = functions.https.onRequest((request, response) => {
+//	response.send("Hello from Firebase!");
+//});
+const rp = require('request-promise');
+const cors = require('cors')({ origin: true });
+
+exports.checkRecaptcha = functions.https.onRequest((req, res) => {
+	const response = req.query.response;
+	const remoteip = req.query.remoteip;
+	console.log("recaptcha response", response);
+	console.log("recaptcha remoteip", remoteip);
+	rp({
+		uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
+		method: 'POST',
+		formData: {
+			secret: '6LfcJbwUAAAAACKj4qshxdmj20bfzUHwFSdP_npX',
+			response: response,
+			remoteip: remoteip
+		},
+		json: true
+	}).then(result => {
+		console.log("recaptcha result", result)
+		if (result.success) {
+			cors(req, res, () => { res.send("You're good to go, human."); });
+			return true;
+		}
+		else {
+			cors(req, res, () => { res.send("Recaptcha verification failed. Are you a robot?"); });
+			return false;
+		}
+	}).catch(reason => {
+		console.log("Recaptcha request failure", reason);
+		cors(req, res, () => { res.send("Recaptcha request failed."); });
+	})
+})
--- resources/scripts/cb_main.js
+++ resources/scripts/cb_main.js
@@ -1,8 +1,45 @@
+var reCAPTCHA_token = null;
+var ipAddress = null;
+var reCAPTCHA_verified = false;
+
+function onrecaptchaload() {
+	console.log('%c This site is protected by reCAPTCHA.', 'background: #001AFA; color: #FFFFFF ; font-weight:bold;');
+}
+
+function onSubmit(token) {
+	reCAPTCHA_token = token;
+}
+
 window.onload = function () {
 
 	// Set footer date
 	$('#footerYear').html(new this.Date().getFullYear());
 
 	// Initialize materialize components
-	$('.parallax').parallax();
+	//$('.parallax').parallax();
+	$('.modal').modal({ dismissible: false });
+
+	$('#proceedToPay').click(function () {
+		$('#proceedToPay').addClass('disabled');
+		$('#paymentLoader').show();
+		grecaptcha.execute();
+		setTimeout(function () {
+			$.get('https://api.ipify.org', function (data) {
+				ipAddress = data;
+				$.get('https://us-central1-theneighbor-hood.cloudfunctions.net/checkRecaptcha', {
+					"response": reCAPTCHA_token,
+					"remoteip": ipAddress
+				}, function (data, status) {
+					if (status == 'success') {
+						if (data == 'You\'re good to go, human.') {
+							reCAPTCHA_verified = true;
+							// Invalidate reCAPTCHA after two miunutes.
+							setTimeout(function () { reCAPTCHA_verified = false }, 120000);
+						}
+					}
+				});
+			});
+		}, 1000);
+	});
+
 }
